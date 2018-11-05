c8941a93e9c3bdd56c5d3b165d548c4bf31c2e25
onjalipettingill <pet17093@byui.edu>
2018-11-04 22:39:05 -0800

    map aded

--- assignments/lesson-7/scripts/windchill.js
+++ assignments/lesson-7/scripts/windchill.js
@@ -1,9 +1,11 @@
-function myFunction(){
-var wind =  parseInt(document.getElementById("windSpeed").innerHTML);
-var temp =  parseInt(document.getElementById("temp").innerHTML);
-var chill=(0.0817*(3.71*(Math.pow(wind, 0.5))+
+
+var wind = 5;
+var temp = 76;
+var chill = (0.0817*(3.71*(Math.pow(wind, 0.5))+
 5.81-0.25*wind)*(temp-91.4)+91.4);
 
 document.getElementById("output").innerHTML = chill;
-}
+
+
+
 
