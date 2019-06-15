30c65d0fe6776482c0400ff9542c30f2f9cf239d
onjalipettingill <pet17093@byui.edu>
2019-06-15 07:38:09 -0700

    chchchange

--- assignments/lesson-8/scripts/windchill.js
+++ assignments/lesson-8/scripts/windchill.js
@@ -4,7 +4,7 @@ var temp = 76;
 var chill = (0.0817*(3.71*(Math.pow(wind, 0.5))+
 5.81-0.25*wind)*(temp-91.4)+91.4);
 
-document.getElementById("output").innerHTML = chill;
+document.getElementById("windchilloutput").innerHTML = chill;
 
 
 
