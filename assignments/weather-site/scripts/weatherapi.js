ff19fc108afa7470bcae38f8de454ceb57d4a0c1
onjalipettingill <pet17093@byui.edu>
2019-07-05 21:45:37 -0700

    k

--- assignments/weather-site/scripts/weatherapi.js
+++ assignments/weather-site/scripts/weatherapi.js
@@ -19,3 +19,27 @@ let desc = weatherData.weather[0].description;
 
 
 }
+
+//-----------Soda Springs
+
+let weatherRequestFH = new XMLHttpRequest();
+let apiFHURLstring =
+'https://api.openweathermap.org/data/2.5/weather?id=5585010&units=imperial&APPID=9f0d8ba5071db5ba05fc4a7a0b1c086b';
+
+weatherRequestFH.open("get" , apiFHURLstring , true);
+weatherRequestFH.send();
+
+weatherRequestFH.onload =  function () {
+    let weatherData2 = JSON.parse(weatherRequestFH.responseText);
+    console.log(weatherData2);
+
+
+document.getElementById('current-temp').innerHTML = weatherDataFH.main.temp;
+document.getElementById('currently').innerHTML = weatherDataFH.weather[0].description;
+document.getElementById('humidity').innerHTML = weatherDataFH.main.humidity;
+document.getElementById('windSpeed').innerHTML = weatherDataFH.wind.speed;
+let icon = 'http://openweathermap.org/img/wn/01d@2x.png';
+let desc = weatherDataFH.weather[0].description;
+
+}
+
